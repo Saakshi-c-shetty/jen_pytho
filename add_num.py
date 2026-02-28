15966fbddfacdcbf726c3e203accc9d6aa7a8b4c
Saakshi-c-shetty <shettysaakshic@gmail.com>
2026-02-28 12:38:41 +0530

    Update add_num.py

--- add_num.py
+++ add_num.py
@@ -1,9 +1,1 @@
-# Simple addition script
-num1 = 10
-num2 = 5
-
-# Adding the numbers
-result = num1 + num2
-
-# Displaying the result
-print(f"The sum of {num1} and {num2} is {result}")
+print("hello world ")
